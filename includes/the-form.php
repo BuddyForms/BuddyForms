b702af9d2bc0478f0f1a5fc59914bb43edfc0bff
konradsroka <contact@konradsroka.com>
2013-07-09 00:11:50 +0200

    change da form man!

--- includes/the-form.php
+++ includes/the-form.php
@@ -298,13 +298,14 @@ function buddyforms_create_edit_form( $args = array() ) {
 				if(isset($form_notice))
 					$form->addElement(new Element_HTML($form_notice));
 				
-					$form->addElement(new Element_HTML('<div class="lable">'));
-					$form->addElement(new Element_Textbox("Title:", "editpost_title", array('lable' => 'enter a title', "required" => 1, 'value' => $editpost_title)));
+					$form->addElement(new Element_HTML('<div class="bf_field_group">'));
+					$form->addElement(new Element_Textbox("Title:", "editpost_title", array("required" => 1, 'value' => $editpost_title)));
 					$form->addElement(new Element_HTML('</div>'));
 
+					$form->addElement(new Element_HTML('<div class="label"><label>Content:</label></div>'));
+
 					ob_start();
-						echo '<div class="hans">';
-						echo '<div class="label"><label>Content:</label></div>';
+					
 						$settings = array('wpautop' => true, 'media_buttons' => true, 'wpautop' => true, 'tinymce' => true, 'quicktags' => true, 'textarea_rows' => 18);
 						if(isset($post_id)){
 							wp_editor($editpost_content_val, 'editpost_content', $settings);
@@ -313,7 +314,6 @@ function buddyforms_create_edit_form( $args = array() ) {
 							$post = 0;
 							wp_editor($content, 'editpost_content', $settings);
 						}
-						echo '</div>';
 						$wp_editor = ob_get_contents();
 					ob_clean();
 					
@@ -341,7 +341,7 @@ function buddyforms_create_edit_form( $args = array() ) {
 							} else {
 								$customfield_val = get_post_meta($post_id, $slug, true);
 							}
-							$form->addElement(new Element_HTML('<div class="labal">'));
+	
 							switch( $customfield['type'] ) {
 								case 'Mail' :
 									$element_attr = isset($customfield['required']) ? array('required' => true, 'value' => $customfield_val, 'class' => 'settings-input') : array('value' => $customfield_val, 'class' => 'settings-input');
@@ -423,8 +423,6 @@ function buddyforms_create_edit_form( $args = array() ) {
 									break;
 		
 							}
-							$form->addElement(new Element_HTML('</div>'));
-							
 						endif;
 					endforeach;
 				}
