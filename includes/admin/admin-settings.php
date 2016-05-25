069685b37edd5f2ce7f6b0552fc8344c88d97edf
Sven Lehnert <mail@sven-lehnert.de>
2016-05-25 16:50:48 +0200

    Remove all EDD code. BuddyForms will become a free plugin soon

--- includes/admin/admin-settings.php
+++ includes/admin/admin-settings.php
@@ -271,114 +271,6 @@ function buddyforms_edd_activate_license() {
 
 add_action( 'admin_init', 'buddyforms_edd_activate_license' );
 
-
-/***********************************************
- * Illustrates how to deactivate a license key.
- * This will descrease the site count
- ***********************************************/
-
-function buddyforms_edd_deactivate_license() {
-
-	// listen for our activate button to be clicked
-	if ( isset( $_POST['edd_license_deactivate'] ) ) {
-
-		// run a quick security check
-		if ( ! check_admin_referer( 'buddyforms_edd_nonce', 'buddyforms_edd_nonce' ) ) {
-			return;
-		} // get out if we didn't click the Activate button
-
-		// retrieve the license from the database
-		$license = trim( get_option( 'buddyforms_edd_license_key' ) );
-
-
-		// data to send in our API request
-		$api_params = array(
-			'edd_action' => 'deactivate_license',
-			'license'    => $license,
-			'item_name'  => urlencode( BUDDYFORMS_EDD_ITEM_NAME ), // the name of our product in EDD
-			'url'        => home_url()
-		);
-
-		// Call the custom API.
-		$response = wp_remote_post( BUDDYFORMS_STORE_URL, array( 'timeout'   => 15,
-		                                                         'sslverify' => false,
-		                                                         'body'      => $api_params
-		) );
-
-		// make sure the response came back okay
-		if ( is_wp_error( $response ) ) {
-			return false;
-		}
-
-		// decode the license data
-		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-
-		// $license_data->license will be either "deactivated" or "failed"
-		if ( $license_data->license == 'deactivated' ) {
-			delete_option( 'buddyforms_edd_license_status' );
-		}
-
-	}
-}
-
-add_action( 'admin_init', 'buddyforms_edd_deactivate_license' );
-
-
-/************************************
- * this illustrates how to check if
- * a license key is still valid
- * the updater does this for you,
- * so this is only needed if you
- * want to do something custom
- *************************************/
-
-function buddyforms_edd_check_license() {
-
-	global $wp_version;
-
-	$license = trim( get_option( 'buddyforms_edd_license_key' ) );
-
-	$api_params = array(
-		'edd_action' => 'check_license',
-		'license'    => $license,
-		'item_name'  => urlencode( BUDDYFORMS_EDD_ITEM_NAME ),
-		'url'        => home_url()
-	);
-
-	// Call the custom API.
-	$response = wp_remote_post( BUDDYFORMS_STORE_URL, array( 'timeout'   => 15,
-	                                                         'sslverify' => false,
-	                                                         'body'      => $api_params
-	) );
-
-	if ( is_wp_error( $response ) ) {
-		return false;
-	}
-
-	$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-
-	if ( $license_data->license == 'valid' ) {
-		echo 'valid';
-		exit;
-		// this license is still valid
-	} else {
-		echo 'invalid';
-		exit;
-		// this license is no longer valid
-	}
-}
-
-
-
-
-//
-// EDD Settings END
-//
-
-
-
-
-
 //
 // GOOGLE reCaptcha
 //
@@ -437,7 +329,7 @@ function buddyforms_process_settings_import() {
 		'post_type'     => 'buddyforms'
 	);
 
-// Insert the post into the database
+	// Insert the post into the database
 	$new_form_id = wp_insert_post( $new_form );
 
 	// Update post meta
