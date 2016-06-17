0725d47c37d13f918a055a83966c84ec82907b85
Sven Lehnert <mail@sven-lehnert.de>
2016-06-17 11:56:44 +0200

    clean up code

--- templates/buddyforms/the-loop.php
+++ templates/buddyforms/the-loop.php
@@ -1,99 +1,100 @@
 <?php
 global $buddyforms, $bp, $the_lp_query, $current_user, $form_slug;
 
-$current_user = wp_get_current_user(); ?>
+$current_user = wp_get_current_user();
 
-	<div id="buddyforms-list-view" class="buddyforms_posts_list">
+?>
 
-		<?php if ( $the_lp_query->have_posts() ) : ?>
+<div id="buddyforms-list-view" class="buddyforms_posts_list">
 
-			<ul class="buddyforms-list" role="main">
+	<?php if ( $the_lp_query->have_posts() ) : ?>
 
-				<?php while ( $the_lp_query->have_posts() ) : $the_lp_query->the_post();
+		<ul class="buddyforms-list" role="main">
 
-					$the_permalink      = get_permalink();
-					$post_status        = get_post_status();
+			<?php while ( $the_lp_query->have_posts() ) : $the_lp_query->the_post();
 
-					$post_status_css    = bf_get_post_status_css_class( $post_status, $form_slug );
-					$post_status_name   = bf_get_post_status_readable( $post_status );
+				$the_permalink      = get_permalink();
+				$post_status        = get_post_status();
 
-					do_action( 'bp_before_blog_post' );
+				$post_status_css    = bf_get_post_status_css_class( $post_status, $form_slug );
+				$post_status_name   = bf_get_post_status_readable( $post_status );
 
-					?>
+				do_action( 'bp_before_blog_post' );
 
-					<li id="bf_post_li_<?php the_ID() ?>" class="bf-submission <?php echo $post_status_css; ?>">
-						<div class="item">
-							<div class="item-avatar">
-								<?php
-								$post_thumbnail = get_the_post_thumbnail( get_the_ID(), array(
-									70,
-									70
-								), array( 'class' => "avatar" ) );
-								$post_thumbnail = apply_filters( 'buddyforms_loop_thumbnail', $post_thumbnail );
-								?>
-								<a href="<?php echo $the_permalink; ?>"><?php echo $post_thumbnail ?></a>
-							</div>
+				?>
 
-							<div class="item-title"><a href="<?php echo $the_permalink; ?>" rel="bookmark"
-							                           title="<?php _e( 'Permanent Link to', 'buddyforms' ) ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-							</div>
-
-							<div class="item-desc"><?php echo get_the_excerpt(); ?></div>
-
-							<?php do_action( 'buddyforms_the_loop_item_last', get_the_ID() ); ?>
-
+				<li id="bf_post_li_<?php the_ID() ?>" class="bf-submission <?php echo $post_status_css; ?>">
+					<div class="item">
+						<div class="item-avatar">
+							<?php
+							$post_thumbnail = get_the_post_thumbnail( get_the_ID(), array(
+								70,
+								70
+							), array( 'class' => "avatar" ) );
+							$post_thumbnail = apply_filters( 'buddyforms_loop_thumbnail', $post_thumbnail );
+							?>
+							<a href="<?php echo $the_permalink; ?>"><?php echo $post_thumbnail ?></a>
 						</div>
 
-						<?php
-						if ( is_user_logged_in() && get_the_author_meta( 'ID' ) == get_current_user_id() ) {
-							ob_start();
-							?>
-							<div class="action">
-								<span><?php _e( 'Created', 'buddyforms' ); ?> <?php the_time( 'F j, Y' ) ?></span>
-								<div class="meta">
-									<div class="item-status"><?php echo $post_status_name; ?></div>
-									<?php bf_post_entry_actions( $form_slug ); ?>
-								</div>
-							</div>
-							<?php
-							$meta_tmp = ob_get_clean();
-							echo apply_filters( 'buddyforms_the_loop_meta_html', $meta_tmp );
-						}
+						<div class="item-title"><a href="<?php echo $the_permalink; ?>" rel="bookmark"
+						                           title="<?php _e( 'Permanent Link to', 'buddyforms' ) ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+						</div>
+
+						<div class="item-desc"><?php echo get_the_excerpt(); ?></div>
+
+						<?php do_action( 'buddyforms_the_loop_item_last', get_the_ID() ); ?>
+
+					</div>
+
+					<?php
+					if ( is_user_logged_in() && get_the_author_meta( 'ID' ) == get_current_user_id() ) {
+						ob_start();
 						?>
-						<?php do_action('buddyforms_the_loop_li_last', get_the_ID()); ?>
-						<div class="clear"></div>
-					</li>
+						<div class="action">
+							<span><?php _e( 'Created', 'buddyforms' ); ?> <?php the_time( 'F j, Y' ) ?></span>
+							<div class="meta">
+								<div class="item-status"><?php echo $post_status_name; ?></div>
+								<?php bf_post_entry_actions( $form_slug ); ?>
+							</div>
+						</div>
+						<?php
+						$meta_tmp = ob_get_clean();
+						echo apply_filters( 'buddyforms_the_loop_meta_html', $meta_tmp );
+					}
+					?>
+					<?php do_action('buddyforms_the_loop_li_last', get_the_ID()); ?>
+					<div class="clear"></div>
+				</li>
 
-					<?php do_action( 'bf_after_loop_item' ) ?>
+				<?php do_action( 'bf_after_loop_item' ) ?>
 
 
-				<?php endwhile; ?>
+			<?php endwhile; ?>
 
-				<div class="navigation">
-					<?php if ( function_exists( 'wp_pagenavi' ) ) : wp_pagenavi();
-					else: ?>
-						<div
-							class="alignleft"><?php next_posts_link( '&larr;' . __( 'Previous Entries', 'buddyforms' ), $the_lp_query->max_num_pages ) ?></div>
-						<div
-							class="alignright"><?php previous_posts_link( __( 'Next Entries ', 'buddyforms' ) . '&rarr;' ) ?></div>
-					<?php endif; ?>
+			<div class="navigation">
+				<?php if ( function_exists( 'wp_pagenavi' ) ) : wp_pagenavi();
+				else: ?>
+					<div
+						class="alignleft"><?php next_posts_link( '&larr;' . __( 'Previous Entries', 'buddyforms' ), $the_lp_query->max_num_pages ) ?></div>
+					<div
+						class="alignright"><?php previous_posts_link( __( 'Next Entries ', 'buddyforms' ) . '&rarr;' ) ?></div>
+				<?php endif; ?>
 
-				</div>
-
-			</ul>
-
-		<?php else : ?>
-
-			<div id="message" class="info">
-				<p><?php _e( 'There were no posts found.', 'buddyforms' ); ?></p>
 			</div>
 
-		<?php endif; ?>
-		<div class="bf_modal">
-			<div style="display: none;"><?php wp_editor( '', 'editpost_content' ); ?></div>
+		</ul>
+
+	<?php else : ?>
+
+		<div id="message" class="info">
+			<p><?php _e( 'There were no posts found.', 'buddyforms' ); ?></p>
 		</div>
+
+	<?php endif; ?>
+
+	<div class="bf_modal">
+		<div style="display: none;"><?php wp_editor( '', 'editpost_content' ); ?></div>
 	</div>
 
-<?php
-
-wp_reset_query();
+</div>
+<?php wp_reset_query();
--- templates/buddyforms/the-table.php
+++ templates/buddyforms/the-table.php
@@ -3,102 +3,108 @@ global $buddyforms, $bp, $the_lp_query, $current_user, $form_slug;
 
 $current_user = wp_get_current_user(); ?>
 
-	<div id="buddyforms-table-view" class="buddyforms_posts_table">
+<div id="buddyforms-table-view" class="buddyforms_posts_table">
 
-		<?php if ( $the_lp_query->have_posts() ) : ?>
+	<?php if ( $the_lp_query->have_posts() ) : ?>
 
-			<table class="table table-striped">
-				<thead>
-				<tr>
-					<th class="created">
-						<span><?php _e( 'Created', 'buddyforms' ); ?></span>
-					</th>
-					<th class="title">
-						<span><?php _e( 'Title', 'buddyforms' ); ?></span>
-					</th>
-					<th class="status">
-						<span><?php _e( 'Status', 'buddyforms' ); ?></span>
-					</th>
+		<table class="table table-striped">
+			<thead>
+			<tr>
+				<th class="created">
+					<span><?php _e( 'Created', 'buddyforms' ); ?></span>
+				</th>
+				<th class="title">
+					<span><?php _e( 'Title', 'buddyforms' ); ?></span>
+				</th>
+				<th class="status">
+					<span><?php _e( 'Status', 'buddyforms' ); ?></span>
+				</th>
+				<?php if ( is_user_logged_in() ) { ?>
 					<th class="actions">
 						<span><?php _e( 'Actions', 'buddyforms' ); ?></span>
 					</th>
-				</tr>
-				</thead>
-				<tbody>
+				<?php } ?>
+			</tr>
+			</thead>
+			<tbody>
 
-				<?php while ( $the_lp_query->have_posts() ) : $the_lp_query->the_post();
+			<?php while ( $the_lp_query->have_posts() ) : $the_lp_query->the_post();
 
-					$the_permalink      = get_permalink();
-					$post_status        = get_post_status();
+				$the_permalink      = get_permalink();
+				$post_status        = get_post_status();
 
-					$post_status_css    = bf_get_post_status_css_class( $post_status, $form_slug );
-					$post_status_name   = bf_get_post_status_readable( $post_status );
+				$post_status_css    = bf_get_post_status_css_class( $post_status, $form_slug );
+				$post_status_name   = bf_get_post_status_readable( $post_status );
 
-					do_action( 'bp_before_blog_post' ) ?>
+				do_action( 'bp_before_blog_post' ) ?>
 
-					<tr id="bf_post_li_<?php the_ID() ?>" class="<?php echo $post_status_css; ?>">
-						<td>
-							<span class="mobile-th"><?php _e( 'Created', 'buddyforms' ); ?></span>
-							<?php the_time( 'F j, Y' ) ?>
-						</td>
-						<td>
-							<span class="mobile-th"><?php _e( 'Title', 'buddyforms' ); ?></span>
-							<a href="<?php echo $the_permalink; ?>" rel="bookmark"
-							   title="<?php _e( 'Permanent Link to', 'buddyforms' ) ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-							<?php do_action( 'buddyforms_the_loop_item_last', get_the_ID() ); ?>
-						</td>
-						<td colspan="2" class="table-wrapper">
-							<table class="table table-inner">
-								<tbody>
-								<tr class="<?php echo $post_status_css; ?>">
-									<td>
-		                                <span class="mobile-th"><?php _e( 'Status', 'buddyforms' ); ?></span>
-										<div class="status-item">
-											<div class="table-item-status"><?php echo $post_status_name ?></div>
-										</div>
-									</td>
+				<tr id="bf_post_li_<?php the_ID() ?>" class="<?php echo $post_status_css; ?>">
+					<td>
+						<span class="mobile-th"><?php _e( 'Created', 'buddyforms' ); ?></span>
+						<?php the_time( 'F j, Y' ) ?>
+					</td>
+					<td>
+						<span class="mobile-th"><?php _e( 'Title', 'buddyforms' ); ?></span>
+						<a href="<?php echo $the_permalink; ?>" rel="bookmark"
+						   title="<?php _e( 'Permanent Link to', 'buddyforms' ) ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+						<?php do_action( 'buddyforms_the_loop_item_last', get_the_ID() ); ?>
+					</td>
+					<td colspan="2" class="table-wrapper">
+						<table class="table table-inner">
+							<tbody>
+							<tr class="<?php echo $post_status_css; ?>">
+								<td>
+	                                <span class="mobile-th"><?php _e( 'Status', 'buddyforms' ); ?></span>
+									<div class="status-item">
+										<div class="table-item-status"><?php echo $post_status_name ?></div>
+									</div>
+								</td>
+								<?php if ( is_user_logged_in() && get_the_author_meta( 'ID' ) == get_current_user_id() ) { ?>
 									<td>
 										<div class="meta">
 											<span class="mobile-th"><?php _e( 'Actions', 'buddyforms' ); ?></span>
 											<?php bf_post_entry_actions($form_slug); ?>
 										</div>
 									</td>
-								</tr>
-								<?php do_action( 'buddyforms_the_table_inner_tr_last', get_the_ID() ); ?>
-								</tbody>
-							</table>
-						</td>
-					</tr>
-					<?php do_action( 'buddyforms_the_table_tr_last', get_the_ID() ); ?>
+								<?php } ?>
+							</tr>
+							<?php do_action( 'buddyforms_the_table_inner_tr_last', get_the_ID() ); ?>
+							</tbody>
+						</table>
+					</td>
+				</tr>
+				<?php do_action( 'buddyforms_the_table_tr_last', get_the_ID() ); ?>
 
 
-					<?php do_action( 'bf_after_loop_item' ) ?>
+				<?php do_action( 'bf_after_loop_item' ) ?>
 
-				<?php endwhile; ?>
+			<?php endwhile; ?>
 
-				<div class="navigation">
-					<?php if ( function_exists( 'wp_pagenavi' ) ) : wp_pagenavi();
-					else: ?>
-						<div
-							class="alignleft"><?php next_posts_link( '&larr;' . __( ' Previous Entries', 'buddyforms' ), $the_lp_query->max_num_pages ) ?></div>
-						<div
-							class="alignright"><?php previous_posts_link( __( 'Next Entries ', 'buddyforms' ) . '&rarr;' ) ?></div>
-					<?php endif; ?>
+			<div class="navigation">
+				<?php if ( function_exists( 'wp_pagenavi' ) ) : wp_pagenavi();
+				else: ?>
+					<div
+						class="alignleft"><?php next_posts_link( '&larr;' . __( ' Previous Entries', 'buddyforms' ), $the_lp_query->max_num_pages ) ?></div>
+					<div
+						class="alignright"><?php previous_posts_link( __( 'Next Entries ', 'buddyforms' ) . '&rarr;' ) ?></div>
+				<?php endif; ?>
 
-				</div>
-
-				</tbody>
-			</table>
-		<?php else : ?>
-
-			<div id="message" class="info">
-				<p><?php _e( 'There were no posts found.', 'buddyforms' ); ?></p>
 			</div>
 
-		<?php endif; ?>
-		<div class="bf_modal">
-			<div style="display: none;"><?php wp_editor( '', 'editpost_content' ); ?></div>
+			</tbody>
+		</table>
+
+	<?php else : ?>
+
+		<div id="message" class="info">
+			<p><?php _e( 'There were no posts found.', 'buddyforms' ); ?></p>
 		</div>
+
+	<?php endif; ?>
+
+	<div class="bf_modal">
+		<div style="display: none;"><?php wp_editor( '', 'editpost_content' ); ?></div>
 	</div>
-<?php
-wp_reset_query();
+
+</div>
+<?php wp_reset_query();
