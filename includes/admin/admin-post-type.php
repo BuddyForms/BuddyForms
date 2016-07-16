27030db236e536ea7aafc0e331c3053fe45c696f
konradsroka <konrads@themekraft.com>
2016-07-16 16:28:25 +1000

    fix up the little dev version notice in the top right corner in admin pages

--- includes/admin/admin-post-type.php
+++ includes/admin/admin-post-type.php
@@ -374,7 +374,7 @@ function buddyforms_hide_publishing_actions() {
 				jQuery('body').find('h1:first').html('<div style="font-size: 52px; margin-top: -5px; float: left; margin-right: 15px;" class="tk-icon-buddyforms"></div> ' +
 					'BuddyForms ' +
 					'<a href="post-new.php?post_type=buddyforms" class="page-title-action">Add New</a>' +
-					'<small style="font-size: 20px; padding-top: 23px; float:right;" >Version <?php echo BUDDYFORMS_VERSION ?></small>'
+					'<small style="line-height: 1; margin-top: -10px; margin-right: -15px; color: #888; font-size: 13px; padding-top: 23px; float:right;">Version <?php echo BUDDYFORMS_VERSION ?></small>'
 				);
 				jQuery('h1').show();
 			});
