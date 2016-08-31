0afa843bc477ca33a1c43814bc5ae8e604267487
konradsroka <konrads@themekraft.com>
2016-08-31 23:37:39 +1000

    added ID to the buddyforms admin header h1

--- includes/admin/bf-admin-header.php
+++ includes/admin/bf-admin-header.php
@@ -1,4 +1,4 @@
-<h1 style="line-height: 58px; font-size: 30px; display: block;">
+<h1 id="buddyforms-adminhead" style="line-height: 58px; font-size: 30px; display: block;">
 	<div style="font-size: 52px; margin-top: -5px; float: left; margin-right: 15px;" class="tk-icon-buddyforms"></div>
 	BuddyForms
 	<small style="line-height: 1; margin-top: -10px; margin-right: -15px; color: #888; font-size: 13px; padding-top: 23px;">Version <?php echo BUDDYFORMS_VERSION ?></small>
