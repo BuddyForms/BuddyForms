97df718808a488371186754d27da0ee68f2ce41d
Sven Lehnert <mail@sven-lehnert.de>
2016-09-20 18:07:59 +0200

    only display capabilities if post is saved with a post name

--- includes/admin/form-builder/meta-boxes/metabox-permissions.php
+++ includes/admin/form-builder/meta-boxes/metabox-permissions.php
@@ -10,7 +10,7 @@ function buddyforms_permissions_unregistered_screen() {
 		$buddyform = get_post_meta( get_the_ID(), '_buddyforms_options', true );
 	}
 
-	echo '<h4>Unregistered User</h4><br>';
+	echo '<h4>' . __('Unregistered User', 'buddyforms') . '</h4><br>';
 
 	$public_submit = '';
 	if ( isset( $buddyform['public_submit'] ) ) {
@@ -58,16 +58,14 @@ function buddyforms_permissions_screen() {
 		$buddyform = get_post_meta( get_the_ID(), '_buddyforms_options', true );
 	}
 
-	echo '<br><br><h4>Logged in User</h4><br>';
+	echo '<br><br><h4>' . __('Logged in User', 'buddyforms') . '</h4><br>';
 
 	echo '<div class="bf-roles-main-desc">
-			<div class="">
-			<p>' . __( 'Control who can create, edit and delete content that is created from this form for each user role. If you want to create additional custom user roles, we recommend the Members plugin.', 'buddyforms' ) . '</p>
-			<p><b>' . __( 'Check/Uncheck capabilities to allow/disallow users to create, edit and/or delete posts of this form', 'buddyforms' ) . '</b></p><p><a href="#" class="bf_check_all">' . __( 'Check all', 'buddyforms' ) . '</a></p></div></div>';
-
-
-//		$form_setup[] = new Element_HTML();
-
+			<div>
+				<p>' . __( 'Control who can create, edit and delete content that is created from this form for each user role. If you want to create additional custom user roles, we recommend the Members plugin.', 'buddyforms' ) . '</p>
+				<p><b>' . __( 'Check/Uncheck capabilities to allow/disallow users to create, edit and/or delete posts of this form', 'buddyforms' ) . '</b></p><p><a href="#" class="bf_check_all">' . __( 'Check all', 'buddyforms' ) . '</a></p>
+				</div>
+			</div>';
 
 		foreach ( get_editable_roles() as $role_name => $role_info ):
 
@@ -123,8 +121,8 @@ function buddyforms_permissions_screen() {
 				<table class="wp-list-table widefat posts striped bf_permissions">
 					<thead>
 						<tr>
-							<th class="field_label">Role</th>
-							<th class="field_name">Create - Edit - Delete</th>
+							<th class="field_label"><?php _e('Role', 'buddyforms') ?></th>
+							<th class="field_name"><?php _e('Create - Edit - Delete', 'buddyforms') ?></th>
 						</tr>
 					</thead>
 					<tbody id="the-list">
@@ -167,11 +165,21 @@ function buddyforms_form_setup_nav_li_permission(){ ?>
 }
 add_action('buddyforms_form_setup_nav_li_last', 'buddyforms_form_setup_nav_li_permission');
 
-function buddyforms_form_setup_tab_pane_permission(){ ?>
+function buddyforms_form_setup_tab_pane_permission(){
+	global $post; ?>
 	<div class="tab-pane fade in" id="permission">
 		<div class="buddyforms_accordion_permission">
 			<?php buddyforms_permissions_unregistered_screen() ?>
-			<?php buddyforms_permissions_screen() ?>
+			<?php
+
+			if( !empty($post->post_name) ) {
+				buddyforms_permissions_screen();
+			} else {
+				echo '<br><br><h4>' . __('Logged in User', 'buddyforms') . '</h4><br>';
+				echo '<p>' . __( 'Please enter a form title and save the form to creating permissions. Capabilities are created form the post name instead of the id to keep capabilities in a readable format.', 'buddyforms' ) . '</p>';
+
+			}
+			?>
 		</div>
 	</div><?php
 }
