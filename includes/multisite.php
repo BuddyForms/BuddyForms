53ca8365c776c0bc28260a7a2c6e3655b64bce14
Sven Lehnert <mail@sven-lehnert.de>
2016-10-22 17:25:05 +0200

    new function buddyforms_is_multisite

---
+++ includes/multisite.php
@@ -0,0 +1,27 @@
+<?php
+
+function buddyforms_is_multisite(){
+	if( is_multisite() ) {
+		if ( apply_filters( 'buddyforms_enable_multisite', false ) ) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function buddyforms_switch_to_form_blog($form_slug){
+	global $buddyforms;
+
+	// return if not a network install
+	if( !buddyforms_is_multisite() ){
+		return false;
+	}
+
+	// Check if the form has a blog id to switch to
+	if ( isset( $buddyforms[$form_slug]['blog_id'] ) ) {
+		switch_to_blog( $buddyforms[$form_slug]['blog_id'] );
+		return true;
+	}
+
+	return false;
+}
