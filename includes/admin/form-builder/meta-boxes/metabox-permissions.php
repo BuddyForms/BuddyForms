7273ad813ff82cef3086a33dc8ca4363629ca781
Sven Lehnert <mail@sven-lehnert.de>
2016-10-27 10:51:22 +0200

    Access for unregistered users was always checked

--- includes/admin/form-builder/meta-boxes/metabox-permissions.php
+++ includes/admin/form-builder/meta-boxes/metabox-permissions.php
@@ -12,8 +12,8 @@ function buddyforms_permissions_unregistered_screen() {
 
 	echo '<h4>' . __( 'Unregistered User', 'buddyforms' ) . '</h4><br>';
 
-	$public_submit = isset( $buddyform['public_submit'] ) ? '' : 'public_submit';
-	$form_setup[]  = new Element_Checkbox( '<b>' . __( 'Public Submittable', 'buddyforms' ) . '</b>', "buddyforms_options[public_submit]", array( 'public_submit' => __( 'Deactivate access for unregistered users.', 'buddyforms' ) ), array(
+	$public_submit = isset( $buddyform['public_submit'] ) ? $buddyform['public_submit'] : '';
+	$form_setup[]  = new Element_Checkbox( '<b>' . __( 'Public Submittable', 'buddyforms' ) . '</b>', "buddyforms_options[public_submit]", array( 'public_submit' => __( 'Access for unregistered users.', 'buddyforms' ) ), array(
 		'value'     => $public_submit,
 		'shortDesc' => 'Please use the CAPTCHA form element if this option is enabled to make sure you site is save.'
 	) );
