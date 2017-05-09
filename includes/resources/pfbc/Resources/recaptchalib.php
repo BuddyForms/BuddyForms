e3b32b40eebc13bd24855a0c36484ae795d514cc
Sven Lehnert <mail@sven-lehnert.de>
2017-05-09 11:28:32 +0200

    Added https support to google recaptcha

--- includes/resources/pfbc/Resources/recaptchalib.php
+++ includes/resources/pfbc/Resources/recaptchalib.php
@@ -35,11 +35,11 @@
 /**
  * The reCAPTCHA server URL's
  */
-define( "RECAPTCHA_API_SERVER", "http://www.google.com/recaptcha/api" );
+define( "RECAPTCHA_API_SERVER", "//www.google.com/recaptcha/api" );
 /**
  *
  */
-define( "RECAPTCHA_API_SECURE_SERVER", "https://www.google.com/recaptcha/api" );
+define( "RECAPTCHA_API_SECURE_SERVER", "//www.google.com/recaptcha/api" );
 /**
  *
  */
