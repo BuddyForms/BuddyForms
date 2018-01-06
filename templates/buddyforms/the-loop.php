b70b9aaf34d25caa97a92240cda69a650f494185
Konrad Sroka <cunraid@gmail.com>
2018-01-06 17:51:34 +1000

    oh yeah bit better the image like that on mobile

--- templates/buddyforms/the-loop.php
+++ templates/buddyforms/the-loop.php
@@ -44,8 +44,8 @@ if ( ! defined( 'ABSPATH' ) ) {
             <div class="item-thumb">
 							<?php
 							$post_thumbnail = get_the_post_thumbnail( get_the_ID(), array(
-								300,
-								300
+								420,
+								420
 							), array( 'class' => "thumb" ) );
 							$post_thumbnail = apply_filters( 'buddyforms_loop_thumbnail', $post_thumbnail );
 
