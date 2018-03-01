a1b6caf4aad3afa392d893810a1c6c4dd731cb9b
Guillermo <gfirem@gmail.com>
2018-03-01 11:56:04 -0300

    * Adding the interaction when the ajax is in progress to disabled the submit button

--- assets/resources/dropzone/initializer.js
+++ assets/resources/dropzone/initializer.js
@@ -2,7 +2,6 @@
  * Created by Victor on 28/12/2017.
  */
 jQuery(document).ready(function ($) {
-	/* <fs_premium_only> */
 	$(".dropzone").each(function (index, value) {
 		var current = $(this),
 			id = current.attr('id'),
@@ -13,6 +12,7 @@ jQuery(document).ready(function ($) {
 
 		Dropzone.autoDiscover = false;
 		var clickeable = action !== 'edit';
+		var currentField = jQuery('#field_' + id);
 
 		$("#" + id).dropzone({
 			url: dropParam.admin_url,
@@ -20,61 +20,64 @@ jQuery(document).ready(function ($) {
 			acceptedFiles: accepted_files,
 			maxFiles: multiple_files,
 			clickable: clickeable,
-			sending: function(file, xhr, formData){
-				formData.append('action', 'handle_dropped_media' );
-				formData.append('nonce', dropParam.ajaxnonce );
-			},
-			success: function (file, response) {
-				file.previewElement.classList.add("dz-success");
-				file['attachment_id'] = response; // push the id for future reference
-				var ids = jQuery('#field_' + id).val() + ',' + response;
-				var idsFormat = "";
-				if (ids[0] === ',') {
-					idsFormat = ids.substring(1, ids.length);
-				} else {
-					idsFormat = ids;
-				}
-
-				jQuery('#field_' + id).val(idsFormat);
-			},
-			error: function (file, response) {
-				file.previewElement.classList.add("dz-error");
-				jQuery(file.previewElement).find('div.dz-error-message>span').text(response);
-				console.log('error', response);
-			},
-			maxfilesreached: function (file) {
-				console.log('max file reached', file);
-			},
-			// update the following section is for removing image from library
 			addRemoveLinks: true,
-			removedfile: function (file) {
-				var attachment_id = file.attachment_id;
-				var ids = jQuery('#field_' + id).val();
+			init: function () {
+				this.on('complete', function () {
+					jQuery("button[type=submit].bf-submit").removeAttr("disabled");
+				});
 
-				var remainigIds = ids.replace(attachment_id, "");
-				if (remainigIds[0] === ',') {
-					remainigIds = remainigIds.substring(1, ids.length);
-				}
-				var lastChar = remainigIds[remainigIds.length - 1];
-				if (lastChar === ',') {
-					remainigIds = remainigIds.slice(0, -1);
-				}
+				this.on('addedfile', function () {
+					jQuery("button[type=submit].bf-submit").attr("disabled", "disabled");
+				});
 
-				jQuery('#field_' + id).val(remainigIds);
-				jQuery.ajax({
-					type: 'POST',
-					url: dropParam.admin_url,
-					data: {
+				this.on('sending', function (file, xhr, formData) {
+					formData.append('action', 'handle_dropped_media');
+					formData.append('nonce', dropParam.ajaxnonce);
+				});
+
+				this.on('success', function (file, response) {
+					file.previewElement.classList.add("dz-success");
+					file['attachment_id'] = response; // push the id for future reference
+					var ids = currentField.val() + ',' + response;
+					var idsFormat = "";
+					if (ids[0] === ',') {
+						idsFormat = ids.substring(1, ids.length);
+					} else {
+						idsFormat = ids;
+					}
+					currentField.val(idsFormat);
+				});
+
+				this.on('error', function (file, response) {
+					file.previewElement.classList.add("dz-error");
+					jQuery(file.previewElement).find('div.dz-error-message>span').text(response);
+					jQuery("button[type=submit].bf-submit").removeAttr("disabled");
+				});
+				this.on('removedfile', function (file) {
+					var attachment_id = file.attachment_id;
+					var ids = currentField.val();
+					var remainigIds = ids.replace(attachment_id, "");
+					if (remainigIds[0] === ',') {
+						remainigIds = remainigIds.substring(1, ids.length);
+					}
+					var lastChar = remainigIds[remainigIds.length - 1];
+					if (lastChar === ',') {
+						remainigIds = remainigIds.slice(0, -1);
+					}
+					currentField.val(remainigIds);
+					jQuery("button[type=submit].bf-submit").attr("disabled", "disabled");
+					jQuery.post(dropParam.admin_url, {
 						action: 'handle_deleted_media',
 						media_id: attachment_id,
 						nonce: dropParam.ajaxnonce
-					}
+					}, function (data) {
+						console.log(data);
+					}).always(function () {
+						jQuery("button[type=submit].bf-submit").removeAttr("disabled");
+					});
 				});
-				var _ref;
-				return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) : void 0;
 			}
 		});
 
 	});
-	/* </fs_premium_only> */
 });
