760ea712773ac79e80db94bf3b40e0392266c614
Guillermo Figueroa <gfirem@gmail.com>
2018-11-21 15:04:39 -0300

    removing unnecessary function.
    https://secure.helpscout.net/conversation/639291131/23050/?folderId=1428166

--- includes/functions.php
+++ includes/functions.php
@@ -829,12 +829,6 @@ function buddyforms_get_all_pages( $type = 'id', $view = "form_builder" ) {
 	return $all_pages;
 }
 
-// Set the js and css to global by default. There are to many issues with the function at the moment. Can still set to false with the filer priority higher than 10 or by de register buddyforms_front_js_css_loader_global
-add_filter( 'buddyforms_front_js_css_loader', 'buddyforms_front_js_css_loader_global', 1, 10 );
-function buddyforms_front_js_css_loader_global( $found ) {
-	return true;
-}
-
 add_action( 'admin_bar_menu', 'buddyform_admin_bar_shortcut', 60 );
 /**
  * Add a short-code to the admin toolbar to edit the form in the current screen
