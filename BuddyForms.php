ce8997f9358e27f9fb5e6c894732c02fb7150b7e
svenl77 <mail@sven-lehnert.de>
2019-01-21 11:35:06 +0000

    update freemius to latest version
    new BuddyForms Version 2.3

--- BuddyForms.php
+++ BuddyForms.php
@@ -4,7 +4,7 @@
  * Plugin Name: BuddyForms
  * Plugin URI:  https://themekraft.com/buddyforms/
  * Description: Contact Forms, Post Forms for User Generated Content and Registration Forms easily build in minutes. Step by step with an easy to use Form Wizard. Ideal for User Submitted Posts. Extendable with Addons!
- * Version: 2.2.9.2
+ * Version: 2.3
  * Author: ThemeKraft
  * Author URI: https://themekraft.com/buddyforms/
  * Licence: GPLv3
@@ -44,7 +44,7 @@ if ( ! class_exists( 'BuddyForms' ) ) {
 		/**
 		 * @var string
 		 */
-		public $version = '2.2.9.2';
+		public $version = '2.3';
 
 		/**
 		 * @var string Assets URL
