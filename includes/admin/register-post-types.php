db190a59a55de6e234175e386f50f305088c678e
gfirem <gfirem@gmail.com>
2019-03-17 23:15:12 -0300

    * Added a container to divide the form actions.

--- includes/admin/register-post-types.php
+++ includes/admin/register-post-types.php
@@ -571,25 +571,29 @@ function buddyforms_add_button_to_submit_box() {
 	?>
     <div id="buddyforms-actions" class="misc-pub-section">
 		<?php if ( isset( $post->post_name ) && $post->post_name != '' ) { ?>
-            <div id="frontend-actions">
-                <a class="button button-large bf_button_action" target="_blank"
-                   href="<?php echo $base ?>/?page_id=<?php echo $preview_page_id ?>&preview=true&form_slug=<?php echo $post->post_name ?>"><span
-                            class="dashicons dashicons-visibility"></span> <?php _e( 'Preview Form', 'buddyforms' ) ?>
-                </a>
-            </div>
+			<div id="frontend-actions">
+				<a class="button button-large bf_button_action" target="_blank"
+				   href="<?php echo $base ?>/?page_id=<?php echo $preview_page_id ?>&preview=true&form_slug=<?php echo $post->post_name ?>"><span
+						class="dashicons dashicons-visibility"></span> <?php _e('Preview Form', 'buddyforms') ?>
+				</a>
+			</div>
 		<?php } ?>
 		<?php if ( isset( $buddyform['attached_page'] ) && isset( $buddyform['post_type'] ) && $buddyform['attached_page'] != 'none' ) { ?>
-            <div id="frontend-actions">
-                <label for="button"><?php _e( 'Frontend', 'buddyforms' ) ?></label>
-				<?php echo '<a class="button button-large bf_button_action" href="' . $attached_page_permalink . 'view/' . $post->post_name . '/" target="_new"><span class="dashicons dashicons-admin-page"></span> ' . __( 'Your Submissions', 'buddyforms' ) . '</a>
-                <a class="button button-large bf_button_action" href="' . $attached_page_permalink . 'create/' . $post->post_name . '/" target="_new"><span class="dashicons dashicons-feedback"></span>    ' . __( 'The Form', 'buddyforms' ) . '</a>'; ?>
-            </div>
+			<div class="bf-tile actions">
+				<div id="frontend-actions">
+					<label for="button"><?php _e('Frontend', 'buddyforms') ?></label>
+					<?php echo '<a class="button button-large bf_button_action" href="' . $attached_page_permalink . 'view/' . $post->post_name . '/" target="_new"><span class="dashicons dashicons-admin-page"></span> ' . __('Your Submissions', 'buddyforms') . '</a>
+                <a class="button button-large bf_button_action" href="' . $attached_page_permalink . 'create/' . $post->post_name . '/" target="_new"><span class="dashicons dashicons-feedback"></span>    ' . __('The Form', 'buddyforms') . '</a>'; ?>
+				</div>
+			</div>
 		<?php }
 		if ( isset( $post->post_name ) && $post->post_name != '' ) { ?>
-            <div id="admin-actions">
-                <label for="button"><?php _e( 'Admin', 'buddyforms' ) ?></label>
-				<?php echo '<a class="button button-large bf_button_action" href="edit.php?post_type=buddyforms&page=buddyforms_submissions&form_slug=' . $post->post_name . '"><span class="dashicons dashicons-email"></span> ' . __( 'Submissions', 'buddyforms' ) . '</a>'; ?>
-            </div>
+			<div class="bf-tile actions">
+				<div id="admin-actions">
+					<label for="button"><?php _e('Admin', 'buddyforms') ?></label>
+					<?php echo '<a class="button button-large bf_button_action" href="edit.php?post_type=buddyforms&page=buddyforms_submissions&form_slug=' . $post->post_name . '"><span class="dashicons dashicons-email"></span> ' . __('Submissions', 'buddyforms') . '</a>'; ?>
+				</div>
+			</div>
 		<?php } ?>
 
         <div class="clear"></div>
