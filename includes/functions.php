9a31cdf4fa3a1176959e6cafc9728ee83de3bbbc
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2019-04-02 01:04:53 -0300

    * Updated the draft text

--- includes/functions.php
+++ includes/functions.php
@@ -1330,7 +1330,7 @@ function buddyforms_form_action_buttons( $form, $form_slug, $post_id, $field_opt
 	$form_type = isset( $buddyforms[ $form_slug ]['form_type'] ) ? $buddyforms[ $form_slug ]['form_type'] : '';
 
 	if ( ! $exist_field_status && $is_draft_enabled && $is_field_draft_enabled && $form_type === 'post' && is_user_logged_in() && $user_can_draft ) {
-		$bf_draft_button_text    = ! empty( $bfdesign['draft_text'] ) ? $bfdesign['draft_text'] : __( 'Draft', 'buddyforms' );
+		$bf_draft_button_text    = ! empty( $bfdesign['draft_text'] ) ? $bfdesign['draft_text'] : __( 'Save as draft', 'buddyforms' );
 		$bf_draft_button_classes = 'bf-draft ' . ! empty( $bfdesign['button_class'] ) ? $bfdesign['button_class'] : '';
 		$bf_draft_button         = new Element_Button( $bf_draft_button_text, 'button', array(
 			'id'          => $form_slug . '-draft',
