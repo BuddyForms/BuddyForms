2dad78d276f01fa875cf302c17ef8d8221c46feb
Victor Marin <marin250189@gmail.com>
2019-06-06 11:37:12 -0400

    Price Field Options

--- includes/resources/pfbc/Element/Price.php
+++ includes/resources/pfbc/Element/Price.php
@@ -15,7 +15,15 @@ class Element_Price extends Element_Textbox {
 	}
 
 	public static function builder_element_options( $form_fields, $form_slug, $field_type, $field_id, $buddyform ) {
+        $thousands_separator     = isset( $buddyform['form_fields'][ $field_id ]['thousands_separator'] ) ? stripslashes( $buddyform['form_fields'][ $field_id ]['thousands_separator'] ) : '.';
+        $prefix = isset( $buddyform['form_fields'][ $field_id ]['prefix'] ) ? $buddyform['form_fields'][ $field_id ]['prefix'] : '$ ';
+        $suffix = isset( $buddyform['form_fields'][ $field_id ]['suffix'] ) ? $buddyform['form_fields'][ $field_id ]['suffix'] : '';
+        $cents_separator = isset( $buddyform['form_fields'][ $field_id ]['cents_separator'] ) ? $buddyform['form_fields'][ $field_id ]['cents_separator'] : ',';
 
+        $form_fields['general']['thousands_separator'] = new Element_Textbox( '<b>' . __( 'Thousands Separator', 'buddyforms' ) . '</b>', "buddyforms_options[form_fields][" . $field_id . "][thousands_separator]", array( 'value' => $thousands_separator ) );
+        $form_fields['general']['prefix'] = new Element_Textbox( '<b>' . __( 'Prefix', 'buddyforms' ) . '</b>', "buddyforms_options[form_fields][" . $field_id . "][prefix]", array( 'value' => $prefix ) );
+        $form_fields['general']['suffix'] = new Element_Textbox( '<b>' . __( 'Suffix', 'buddyforms' ) . '</b>', "buddyforms_options[form_fields][" . $field_id . "][suffix]", array( 'value' => $suffix ) );
+        $form_fields['general']['cents_separator'] = new Element_Textbox( '<b>' . __( 'Cents Separator', 'buddyforms' ) . '</b>', "buddyforms_options[form_fields][" . $field_id . "][cents_separator]", array( 'value' => $cents_separator ) );
 		return $form_fields;
 	}
 }
