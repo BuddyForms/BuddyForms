14056c91b4cec1d31b45e1ca8f92282d49d39682
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2019-12-14 10:30:38 -0300

    New version

--- BuddyForms.php
+++ BuddyForms.php
@@ -4,7 +4,7 @@
  * Plugin Name: BuddyForms
  * Plugin URI:  https://themekraft.com/buddyforms/
  * Description: Contact Forms, Post Forms for User Generated Content and Registration Forms easily build in minutes. Step by step with an easy to use Form Wizard. Ideal for User Submitted Posts. Extendable with Addons!
- * Version: 2.5.9-beta6
+ * Version: 2.5.9
  * Author: ThemeKraft
  * Author URI: https://themekraft.com/buddyforms/
  * Licence: GPLv3
@@ -45,7 +45,7 @@ if ( ! class_exists( 'BuddyForms' ) ) {
 		/**
 		 * @var string
 		 */
-		public $version = '2.5.9-beta6';
+		public $version = '2.5.9';
 
 		/**
 		 * @var array Frontend Global JS parameters
