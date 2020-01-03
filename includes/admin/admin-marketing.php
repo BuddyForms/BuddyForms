3ba86a5995435061e182ab9e8fb46077cb2e38a2
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2020-01-03 10:52:00 -0300

    * Added the create date of the license to the inbound marketing process.

--- includes/admin/admin-marketing.php
+++ includes/admin/admin-marketing.php
@@ -18,51 +18,63 @@ function buddyforms_marketing_init() {
 }
 
 function buddyforms_marketing_assets() {
+	try {
+		$user_id = get_current_user_id();
+		if ( empty( $user_id ) || ! is_user_logged_in() ) {
+			return;
+		}
+		if ( ! current_user_can( 'administrator' ) ) {
+			return;
+		}
 
-	$user_id = get_current_user_id();
-	if ( empty( $user_id ) || ! is_user_logged_in() ) {
-		return;
+		$base_content = "<p class=\"corner-head\">This is only for you</p><p class=\"corner-text\">%content</p><div class=\"bf-marketing-action-container\"><a href=\"#\" class=\"bf-marketing-btn corner-btn-close\">%no</a><a href=\"#\" class=\"bf-marketing-btn corner-btn-close\">%yes</a></div>";
+		$content      = buddyforms_marketing_content_interest();
+		$is_free      = false;
+		$is_pro       = false;
+		$is_trial     = false;
+
+		$first_question = get_user_meta( $user_id, '_buddyforms_marketing_question1', true );
+		$start_date     = get_user_meta( $user_id, '_buddyforms_marketing_start_date', true );
+
+		$freeemius = buddyforms_core_fs();
+		if ( empty( $freeemius ) ) {
+			return;
+		}
+
+		$is_free     = $freeemius->is_free_plan();
+		$is_pro      = $freeemius->is_premium();
+		$is_trial    = $freeemius->is_trial();
+		$upgrade_url = $freeemius->get_upgrade_url();
+		$plugin      = $freeemius->get_plugin();
+
+		if ( $freeemius->has_active_valid_license() ) {
+			$license = $freeemius->_get_license();
+			if ( ! empty( $license ) ) {
+				$created_date = new DateTime( $license->created );
+				//Add an interval to define when to show the modal
+				//Create something generic to handle this generic process like condition meet and then do X
+				//Like a class system where we can override generic behavior using heritage
+			}
+		}
+
+		if ( $is_free ) {
+			$content = buddyforms_marketing_content_pro_coupon();
+		} else if ( $is_trial ) {
+			$content = buddyforms_marketing_content_pro_coupon();
+		} else if ( $is_pro ) {
+
+		}
+
+
+		wp_enqueue_style( 'buddyforms-marketing-popup', BUDDYFORMS_ASSETS . 'resources/corner-popup/css/corner-popup.min.css', array(), BUDDYFORMS_VERSION );
+		wp_enqueue_script( 'buddyforms-marketing-popup', BUDDYFORMS_ASSETS . 'resources/corner-popup/js/corner-popup.min.js', array( 'jquery' ), BUDDYFORMS_VERSION );
+		wp_enqueue_script( 'buddyforms-marketing-popup-handler', BUDDYFORMS_ASSETS . 'admin/js/admin-marketing.js', array( 'jquery' ), BUDDYFORMS_VERSION );
+		wp_localize_script( 'buddyforms-marketing-popup-handler', 'buddyformsMarketingHandler', array(
+			'content' => str_replace( array_keys( $content ), array_values( $content ), $base_content ),
+		) );
+	} catch (Exception $ex){
+
 	}
-	if ( ! current_user_can( 'administrator' ) ) {
-		return;
-	}
-
-	$base_content = "<p class=\"corner-head\">This is only for you</p><p class=\"corner-text\">%content</p><div class=\"bf-marketing-action-container\"><a href=\"#\" class=\"bf-marketing-btn corner-btn-close\">%no</a><a href=\"#\" class=\"bf-marketing-btn corner-btn-close\">%yes</a></div>";
-	$content      = buddyforms_marketing_content_interest();
-	$is_free      = false;
-	$is_pro       = false;
-	$is_trial     = false;
-
-	$first_question = get_user_meta( $user_id, '_buddyforms_marketing_question1', true );
-	$start_date     = get_user_meta( $user_id, '_buddyforms_marketing_start_date', true );
-
-	$freeemius = buddyforms_core_fs();
-	if ( empty( $freeemius ) ) {
-		return;
-	}
-
-	$is_free  = $freeemius->is_free_plan();
-	$is_pro   = $freeemius->is_premium();
-	$is_trial = $freeemius->is_trial();
-	$upgrade_url = $freeemius->get_upgrade_url();
-	$license = $freeemius->_get_license();
-//	$subscription = $freeemius->_get_subscription($license->id);
-
-	if ( $is_free ) {
-		$content = buddyforms_marketing_content_pro_coupon();
-	} else if ( $is_trial ) {
-		$content = buddyforms_marketing_content_pro_coupon();
-	} else if ( $is_pro ) {
-
-	}
-
-
-	wp_enqueue_style( 'buddyforms-marketing-popup', BUDDYFORMS_ASSETS . 'resources/corner-popup/css/corner-popup.min.css', array(), BUDDYFORMS_VERSION );
-	wp_enqueue_script( 'buddyforms-marketing-popup', BUDDYFORMS_ASSETS . 'resources/corner-popup/js/corner-popup.min.js', array( 'jquery' ), BUDDYFORMS_VERSION );
-	wp_enqueue_script( 'buddyforms-marketing-popup-handler', BUDDYFORMS_ASSETS . 'admin/js/admin-marketing.js', array( 'jquery' ), BUDDYFORMS_VERSION );
-	wp_localize_script( 'buddyforms-marketing-popup-handler', 'buddyformsMarketingHandler', array(
-		'content' => str_replace( array_keys( $content ), array_values( $content ), $base_content ),
-	) );
 }
 
 /**
