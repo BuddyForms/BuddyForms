d7de80eae8924f5eae1bc81a48d93c57132e04bc
jnfDev <jairo.jnf.2010@gmail.com>
2020-07-16 14:40:39 -0400

    Fix: add field ID to the buddyforms_fields_inner_loop_start filter.

--- includes/form/form-elements.php
+++ includes/form/form-elements.php
@@ -76,7 +76,7 @@ function buddyforms_form_elements( &$form, $args, $recovering = false ) {
 
 	foreach ( $customfields as $field_id => $customfield ) {
 
-		$form = apply_filters( 'buddyforms_fields_inner_loop_start', $form, $customfield, $form_slug);
+		$form = apply_filters( 'buddyforms_fields_inner_loop_start', $form, $field_id, $customfield, $form_slug);
 
 
 		if ( isset( $customfield['slug'] ) ) {
