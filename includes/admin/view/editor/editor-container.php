64b1e12f0e91f3e782d2d3dbc92d24d3f3f9fccc
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2020-11-18 10:22:35 -0300

    Added a tab and included font awesome

--- includes/admin/view/editor/editor-container.php
+++ includes/admin/view/editor/editor-container.php
@@ -1,6 +1,34 @@
 <div class="tk-container tk-is-fluid">
-  <div class="tk-notification tk-is-primary">
-    This container is <strong>fluid</strong>: it will have a 32px gap on either side, on any
-    viewport size.
-  </div>
+	<div class="tk-notification tk-is-primary">
+		This container is <strong>fluid</strong>: it will have a 32px gap on either side, on any
+		viewport size.
+	</div>
+	<div class="tk-tabs tk-is-toggle tk-is-fullwidth">
+		<ul>
+			<li class="tk-is-active">
+				<a>
+					<span class="tk-icon tk-is-small"><i class="tk-fas tk-fa-image" aria-hidden="true"></i></span>
+					<span>Pictures</span>
+				</a>
+			</li>
+			<li>
+				<a>
+					<span class="tk-icon tk-is-small"><i class="tk-fas tk-fa-music" aria-hidden="true"></i></span>
+					<span>Music</span>
+				</a>
+			</li>
+			<li>
+				<a>
+					<span class="tk-icon tk-is-small"><i class="tk-fas tk-fa-film" aria-hidden="true"></i></span>
+					<span>Videos</span>
+				</a>
+			</li>
+			<li>
+				<a>
+					<span class="tk-icon tk-is-small"><i class="tk-far tk-fa-file-alt" aria-hidden="true"></i></span>
+					<span>Documents</span>
+				</a>
+			</li>
+		</ul>
+	</div>
 </div>
