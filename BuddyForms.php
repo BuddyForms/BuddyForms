2a379851f1236e57d202ffae3d3c1fba81c5b40d
Camilo Luna <camilo@alunizar.co>
2025-05-09 17:09:14 -0500

    feat: remove go pro screen

--- BuddyForms.php
+++ BuddyForms.php
@@ -418,7 +418,6 @@ if ( ! class_exists( 'BuddyForms' ) ) {
 				require_once BUDDYFORMS_INCLUDES_PATH . '/admin/form-builder/form-templates.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . '/admin/admin-ajax.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . '/admin/welcome-screen.php';
-				require_once( BUDDYFORMS_INCLUDES_PATH . '/admin/pricing-screen.php' );
 				require_once BUDDYFORMS_INCLUDES_PATH . '/admin/submissions.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . '/admin/settings.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . '/admin/password-strengh-settings.php';
--- assets/admin/js/admin-all.js
+++ assets/admin/js/admin-all.js
@@ -29,7 +29,7 @@ jQuery(document).ready(function (jQuery) {
     jQuery(".bf-welcome-accordion_tab").click(function(){
 
         jQuery(".bf-welcome-accordion_tab").not(this).each(function(){
-            
+
             jQuery(this).parent().removeClass("active");
             jQuery(this).removeClass("active");
 
@@ -47,118 +47,6 @@ jQuery(document).ready(function (jQuery) {
     var goPro = jQuery('a[href="edit.php?post_type=buddyforms&page=buddyforms_bundle_screen"]');
     goPro.css('color', '#fca300' );
     goPro.parent().insertAfter('#menu-posts-buddyforms > ul > li:last-child');
-
-    jQuery('#purchase').on('click', function (e) {
-
-        var handler = FS.Checkout.configure({
-            plugin_id:  '391',
-            plan_id:    '583',
-            public_key: 'pk_d462eaeb50bc258e3d97c2c146eb6',
-        });
-        
-        handler.open({
-            name     : 'ThemeKraft Bundle',
-            licenses : jQuery('#licenses-1').val(),
-            purchaseCompleted  : function (response) {
-            },
-            success  : function (response) {
-            }
-        });
-        e.preventDefault();
-    });
-
-    jQuery('#purchase-2').on('click', function (e) {
-
-        var handler = FS.Checkout.configure({
-            plugin_id:  '7487',
-            plan_id:    '12239',
-            public_key: 'pk_68d9aeacd7352d37de451d91e3081',
-        });
-        
-        handler.open({
-            name     : 'ThemeKraft Bundle',
-            licenses : jQuery('#licenses-2').val(),
-            purchaseCompleted  : function (response) {
-            },
-            success  : function (response) {
-            }
-        });
-        e.preventDefault();
-    });
-
-    jQuery('#purchase-3').on('click', function (e) {
-
-        var handler = FS.Checkout.configure({
-            plugin_id:  '2046',
-            plan_id:    '4316',
-            public_key: 'pk_ee958df753d34648b465568a836aa',
-        });
-        
-        handler.open({
-            name     : 'ThemeKraft Bundle',
-            licenses : jQuery('#licenses-3').val(),
-            purchaseCompleted  : function (response) {
-            },
-            success  : function (response) {
-            }
-        });
-        e.preventDefault();
-    });
-
-    jQuery("select#licenses-1").change(function () {
-        var selectedCountry = jQuery(this).children("option:selected").val();
-        if( selectedCountry == '1'){
-            jQuery('.fs-bundle-price-1').text('39.99');
-            jQuery('#savings-price').text('59.99');
-        }
-        if( selectedCountry == '5'){
-            jQuery('.fs-bundle-price-1').text('69.99');
-            jQuery('#savings-price').text('199.95');
-        }
-        if( selectedCountry == 'unlimited'){
-            jQuery('.fs-bundle-price-1').text('79.99');
-            jQuery('#savings-price').text('219.99');
-        }
-    });
-
-    jQuery("select#licenses-2").change(function () {
-        var selectedCountry = jQuery(this).children("option:selected").val();
-        if( selectedCountry == '1'){
-            jQuery('.fs-bundle-price-2').text('89.99');
-            jQuery('#savings-price-2').text('342.84');
-        }
-        if( selectedCountry == '5'){
-            jQuery('.fs-bundle-price-2').text('99.99');
-            jQuery('#savings-price-2').text('525.84');
-        }
-        if( selectedCountry == 'unlimited'){
-            jQuery('.fs-bundle-price-2').text('119.99');
-            jQuery('#savings-price-2').text('688.84');
-        }
-    });
-
-    jQuery("select#licenses-3").change(function () {
-        var selectedCountry = jQuery(this).children("option:selected").val();
-        if( selectedCountry == '1'){
-            jQuery('.fs-bundle-price-3').text('99.99');
-            jQuery('#savings-price-3').text('602.75');
-        }
-        if( selectedCountry == '5'){
-            jQuery('.fs-bundle-price-3').text('119.99');
-            jQuery('#savings-price-3').text('965.75');
-        }
-        if( selectedCountry == 'unlimited'){
-            jQuery('.fs-bundle-price-3').text('129.99');
-            jQuery('#savings-price-3').text('1168.76');
-        }
-    });
-
-    jQuery( ".bundle-list-see-more" ).click(function() {
-        jQuery(".list-bundle-ul").animate({
-            height: "1200"
-        });
-        jQuery(".separator, .bundle-list-see-more").hide();
-    });
 });
 
 
