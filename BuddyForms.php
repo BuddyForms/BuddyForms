c5d0c47d3ac3088115f421d8517d5a8715e52caf
Camilo Luna <camilo@alunizar.co>
2025-05-23 14:41:20 -0500

    feat: add submodule admin/pricing-page

--- BuddyForms.php
+++ BuddyForms.php
@@ -416,6 +416,7 @@ if ( ! class_exists( 'BuddyForms' ) ) {
 				require_once BUDDYFORMS_INCLUDES_PATH . 'admin/form-builder/form-builder-elements.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . 'admin/form-builder/form-templates.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . 'admin/admin-ajax.php';
+				require_once BUDDYFORMS_INCLUDES_PATH . 'admin/pricing-page/pricing-page.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . 'admin/welcome-screen.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . 'admin/submissions.php';
 				require_once BUDDYFORMS_INCLUDES_PATH . 'admin/settings.php';
--- RoboFile.php
+++ RoboFile.php
@@ -1,6 +1,6 @@
 <?php
 
-include '.tk/RoboFileBase.php';
+require '.tk/RoboFileBase.php';
 
 class RoboFile extends RoboFileBase {
 
@@ -47,5 +47,52 @@ class RoboFile extends RoboFileBase {
 	 */
 	public function sassLibraryDirectory() {
 		return 'scss/library';
+	}
+
+	/**
+	 * Update git submodules and commit changes if any.
+	 */
+	public function submodulesUpdate() {
+		$submodule_url  = 'git@github.com:Themekraft/pricing-page.git';
+		$submodule_path = 'includes/admin/pricing-page';
+		$this->say( 'Starting submodule update process...' );
+
+		// Check if submodule already exists.
+		$submodule_status_result = $this->taskExec( "git submodule status {$submodule_path}" )
+			->printOutput( false )
+			->run();
+
+		// If the command was successful, the submodule exists
+		$is_submodule_present = $submodule_status_result->wasSuccessful();
+
+		if ( ! $is_submodule_present ) {
+			// Add submodule if it doesn't exist.
+			$this->say( "Adding new submodule: {$submodule_url} -> {$submodule_path}" );
+			$result = $this->taskExec( "git submodule add --force {$submodule_url} {$submodule_path}" )
+				->run();
+			if ( ! $result->wasSuccessful() ) {
+				$this->say( 'Failed to add submodule!' );
+				return;
+			}
+			$this->say( 'Submodule added successfully!' );
+		} else {
+			$this->say( 'Submodule already exists, proceeding with update...' );
+		}
+
+		// Initialize and update submodules.
+		$this->say( 'Initializing and updating submodules...' );
+		$init_result = $this->taskExec( 'git submodule init' )
+			->run();
+		if ( ! $init_result->wasSuccessful() ) {
+			$this->say( 'Failed to initialize submodules!' );
+			return;
+		}
+		$update_result = $this->taskExec( 'git submodule update --remote --merge' )
+			->run();
+		if ( ! $update_result->wasSuccessful() ) {
+			$this->say( 'Failed to update submodules!' );
+			return;
+		}
+		$this->say( 'Submodules updated successfully!' );
 	}
 }
