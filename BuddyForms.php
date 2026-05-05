4b1dbf5ff782709937651d2c4444b941ef6c320e
Camilo Luna <camilo@alunizar.co>
2026-05-04 20:10:42 -0700

    build(beta): version 2.10.0-beta.2
    Bumps pricing-page submodule to pull the orange Go Pro coloring.

--- BuddyForms.php
+++ BuddyForms.php
@@ -4,7 +4,7 @@
  * Plugin Name: BuddyForms
  * Plugin URI:  https://themekraft.com/buddyforms/
  * Description: Contact Forms, Post Forms for User Generated Content and Registration Forms easily build in minutes. Extendable with Addons!
- * Version: 2.10.0-beta.1
+ * Version: 2.10.0-beta.2
  * Author: ThemeKraft
  * Author URI: https://themekraft.com/buddyforms/
  * Licence: GPLv3
@@ -45,7 +45,7 @@ if ( ! class_exists( 'BuddyForms' ) ) {
 		/**
 		 * @var string
 		 */
-		public $version = '2.10.0-beta.1';
+		public $version = '2.10.0-beta.2';
 
 		/**
 		 * @var array Frontend Global JS parameters
